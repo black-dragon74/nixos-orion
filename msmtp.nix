c54338277199340ff0c9f31d5dfd27c271903347
Nick <nickk.2974@gmail.com>
2025-02-08 03:46:39 +0530

    Disable IPv6 temp addressing and enable smartd mons
    Keeping IPv6 privacy extensions enabled leads to rotating IPv6
    addresses. That is not ideal on a server environment.
    
    Moreover, the DDNS service fails to determine the GUA and instead
    incorrectly uses the temporary GUA, which leads to failure in routing.
    
    This patch also enables smartd monitoring and notifications using msmtp.
    
    Signed-off-by: Nick <nickk.2974@gmail.com>

---
+++ msmtp.nix
@@ -0,0 +1,20 @@
+{ pkgs, ...}:
+
+{
+  programs.msmtp = {
+    enable = true;
+    defaults = {
+      port = 465;
+      tls = true;
+      tls_starttls = false;
+    };
+    accounts.default = {
+      auth = true;
+      host = "mailer.srv01-prod.ydvnick.cc";
+      from = "msmtp@mailer.srv01-prod.ydvnick.cc";
+      domain = "mailer.srv01-prod.ydvnick.cc";
+      user = "mailer";
+      passwordeval = "cat /secrets/msmtp_creds.txt";
+    };
+  };
+}
